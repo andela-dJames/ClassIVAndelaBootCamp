fd52ec85cd8a8d0515ee900e37c4a56915769263
Daniel James <danyjayali@hotmail.com>
2015-06-12 11:26:11 +0100

    Create reversestring.js

---
+++ labs/reversestring.js
@@ -0,0 +1,15 @@
+function reverseString(str) {
+  var output = str.split("").reverse().join(''); 
+    if (str.length === 0){
+      return null;
+    }
+     else if (str === isNaN) {
+      return true;
+    }
+     else if (str === output) {
+      return true;
+    }
+      else {
+      return output;
+    }   
+}
